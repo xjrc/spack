f85f819ea4c889ca711e086b8899cd23dba7f05d
Peer-Timo Bremer <bremer5@llnl.gov>
2015-08-21 15:57:34 -0700

    initial packaegs for OpenCV

---
+++ var/spack/packages/OpenCV/package.py
@@ -0,0 +1,27 @@
+from spack import *
+
+class Opencv(Package):
+    """OpenCV (Open Source Computer Vision Library) is an open source
+    computer vision and machine learning software library. OpenCV was
+    built to provide a common infrastructure for computer vision
+    applications and to accelerate the use of machine perception in
+    the commercial products."""
+
+    homepage = "http://opencv.org"
+
+    version('3.0.0',   git='https://github.com/Itseez/opencv.git',commit='c12243c')
+    version('2.4.12.1',git='https://github.com/Itseez/opencv.git',commit='d0210f5')
+    version('2.4.11',  git='https://github.com/Itseez/opencv.git',commit='2c9547e')
+    version('2.4.10.4',git='https://github.com/Itseez/opencv.git',commit='00d9f69')
+    version('2.4.9',   git='https://github.com/Itseez/opencv.git',commit='df8e282')
+
+    depends_on("gcc@4.4:")
+    depends_on("cmake@2.6:")
+    depends_on("pkg-config")
+    depends_on("gtkplus@2.0:")
+    
+    def install(self, spec, prefix):
+        with working_dir('spack-build', create=True):
+            cmake('..', *std_cmake_args)
+            make()
+            make("install")
--- var/spack/packages/pkg-config/package.py
+++ var/spack/packages/pkg-config/package.py
@@ -1,17 +1,19 @@
 from spack import *
 
 class PkgConfig(Package):
-    """pkg-config is a helper tool used when compiling applications and libraries"""
+    """pkg-config is a helper tool used when compiling applications and libraries."""
     homepage = "http://www.freedesktop.org/wiki/Software/pkg-config/"
     url      = "http://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz"
 
-    version('0.28', 'aa3c86e67551adc3ac865160e34a2a0d')
+    version('0.28'  , 'aa3c86e67551adc3ac865160e34a2a0d')
+    version('0.27.1', '5392b4e3372879c5bf856173b418d6a2')
+    version('0.27'  , '3a4c9feab14b6719afd8904945d9b4e4')
+    version('0.26'  , '47525c26a9ba7ba14bf85e01509a7234')
+    version('0.25'  , 'a3270bab3f4b69b7dc6dbdacbcae9745')
 
-    parallel = False
 
     def install(self, spec, prefix):
-        configure("--prefix=%s" %prefix, "--enable-shared")
+        configure('--prefix=%s' % prefix)
 
         make()
         make("install")
-
