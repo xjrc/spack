0829e2311d1fb6af5529088014d1e09c0f274e77
Gregory Becker <becker33@llnl.gov>
2015-09-17 17:24:27 -0700

    updated unittest for slight changes to database API (untested)

--- lib/spack/spack/test/database.py
+++ lib/spack/spack/test/database.py
@@ -38,64 +38,74 @@ from spack.database import Database
 class DatabaseTest(unittest.TestCase):
     def setUp(self):
         self.original_db = spack.installed_db
-        spack.installed_db = Database(self.original_db._root,"_test_index.yaml")
-        self.file_path = join_path(self.original_db._root,"_test_index.yaml")
-        if os.path.exists(self.file_path):
-            os.remove(self.file_path)
+        os.rename(self.original_db._index_path,self.original_db._index_path + ".backup")
+        spack.installed_db = Database(self.original_db.root)
+        if os.path.exists(spack.installed_db._index_path):
+            os.remove(spack.installed_db._index_path)
 
     def tearDown(self):
         spack.installed_db = self.original_db
-        os.remove(self.file_path)
+        os.rename(self.original_db._index_path + ".backup",self.original_db._index_path)
 
     def _test_read_from_install_tree(self):
         specs = spack.install_layout.all_specs()
-        spack.installed_db.read_database()
+        spack.installed_db.read()
         spack.installed_db.write()
-        for sph in spack.installed_db._data:
-            self.assertTrue(sph['spec'] in specs)
+        for entry in spack.installed_db._data.values():
+            self.assertTrue(entry.spec in specs)
         self.assertEqual(len(specs),len(spack.installed_db._data))
 
     def _test_remove_and_add(self):
         specs = spack.install_layout.all_specs()
-        spack.installed_db.remove(specs[len(specs)-1])
-        for sph in spack.installed_db._data:
-            self.assertTrue(sph['spec'] in specs[:len(specs)-1])
+        removed_spec = specs[len(specs)-1]
+        spack.installed_db.remove(removed_spec)
+        for entry in spack.installed_db._data.values():
+            self.assertTrue(entry.spec in specs[:len(specs)-1])
         self.assertEqual(len(specs)-1,len(spack.installed_db._data))
+        self.assertTrue(removed_spec.dag_hash not in spack.installed_db._data)
 
-        spack.installed_db.add(specs[len(specs)-1],"")
-        for sph in spack.installed_db._data:
-            self.assertTrue(sph['spec'] in specs)
+        spack.installed_db.add(removed_spec,"")
+        for entry in spack.installed_db._data.values():
+            self.assertTrue(entry.spec in specs)
         self.assertEqual(len(specs),len(spack.installed_db._data))
+        self.assertTrue(removed_spec.dag_hash in spack.installed_db._data)
 
     def _test_read_from_file(self):
-        spack.installed_db.read_database()
+        spack.installed_db.read()
         size = len(spack.installed_db._data)
-        spack.installed_db._data = spack.installed_db._data[1:]
-        os.utime(spack.installed_db._file_path,None)
-        spack.installed_db.read_database()
-        self.assertEqual(size,len(spack.installed_db._data))
 
         specs = spack.install_layout.all_specs()
+        del spack.installed_db._data[specs[0].dag_hash]
+
+        os.utime(spack.installed_db._file_path,None)
+        spack.installed_db.read()
+
+        self.assertEqual(size,len(spack.installed_db._data))
         self.assertEqual(size,len(specs))
-        for sph in spack.installed_db._data:
-            self.assertTrue(sph['spec'] in specs)
+        for entry in spack.installed_db._data.values():
+            self.assertTrue(entry.spec in specs)
+        self.assertTrue(specs[0].dag_hash in spack.installed_db._data)
 
 
     def _test_write_to_file(self):
-        spack.installed_db.read_database()
+        spack.installed_db.read()
         size = len(spack.installed_db._data)
         real_data = spack.installed_db._data
-        spack.installed_db._data = real_data[:size-1]
+
+        specs = spack.install_layout.all_specs()
+        del spack.installed_db._data[specs[0].dag_hash]
+
         spack.installed_db.write()
         spack.installed_db._data = real_data
         os.utime(spack.installed_db._file_path,None)
-        spack.installed_db.read_database()
+        spack.installed_db.read()
         self.assertEqual(size-1,len(spack.installed_db._data))
 
-        specs = spack.install_layout.all_specs()
+
         self.assertEqual(size,len(specs))
-        for sph in spack.installed_db._data:
-            self.assertTrue(sph['spec'] in specs[:size-1])
+        for entry in spack.installed_db._data:
+            self.assertTrue(entry.spec in specs[1:])
+        self.assertTrue(specs[0].dag_hash not in spack.installed_db._data)
 
     def test_ordered_test(self):
         self._test_read_from_install_tree()
